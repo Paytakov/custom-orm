26879f2b4da33b87779c5b741740236e782806e0
Paytakov <emo_paitakov@abv.bg>
2023-01-22 16:52:22 +0200

    add implementation for doInsert method

--- src/main/java/orm/DBContext.java
+++ src/main/java/orm/DBContext.java
@@ -1,10 +1,11 @@
 package orm;
 
+import java.sql.SQLException;
 import java.util.Iterator;
 
 public interface DBContext<E> {
 
-    boolean persist(E entity) throws IllegalAccessException;
+    boolean persist(E entity) throws IllegalAccessException, SQLException;
     Iterator<E> find(Class<E> table);
     Iterator<E> find(Class<E> table, String where);
     E findFirst(Class<E> table);
--- src/main/java/orm/EntityManager.java
+++ src/main/java/orm/EntityManager.java
@@ -1,11 +1,18 @@
 package orm;
 
+import annotation.Column;
+import annotation.Entity;
 import annotation.Id;
 
 import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EntityManager<E> implements DBContext<E> {
 
@@ -16,13 +23,13 @@ public class EntityManager<E> implements DBContext<E> {
     }
 
     @Override
-    public boolean persist(E entity) throws IllegalAccessException {
+    public boolean persist(E entity) throws IllegalAccessException, SQLException {
         Field primaryKey = getIdColumn(entity.getClass());
         primaryKey.setAccessible(true);
         Object idValue = primaryKey.get(entity);
 
         if (idValue == null || (long) idValue <= 0) {
-           return doInsert(entity, primaryKey);
+            return doInsert(entity, primaryKey);
         }
 
         return doUpdate(entity, primaryKey);
@@ -32,8 +39,61 @@ public class EntityManager<E> implements DBContext<E> {
         return false;
     }
 
-    private boolean doInsert(E entity, Field primaryKey) {
-        return false;
+    private boolean doInsert(E entity, Field primaryKey) throws IllegalAccessException, SQLException {
+        String tableName = getTableName(entity.getClass());
+        String tableFields = getColumnsWithoutId(entity.getClass());
+        String columnValues = getColumnValuesWithoutId(entity);
+
+        String insertQuery = String.format(
+                "INSERT INTO %s (%s) VALUES (%s)",
+                tableName,
+                tableFields,
+                columnValues
+        );
+
+        return connection.prepareStatement(insertQuery).execute();
+    }
+
+    private String getColumnValuesWithoutId(E entity) throws IllegalAccessException {
+        Class<?> aClass = entity.getClass();
+        List<Field> fields = Arrays.stream(aClass.getDeclaredFields())
+                .filter(f -> !f.isAnnotationPresent(Id.class))
+                .filter(f -> f.isAnnotationPresent(Column.class))
+                .toList();
+
+        List<String> values = new ArrayList<>();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(entity);
+
+            if (value instanceof String ||
+                    value instanceof LocalDate) {
+                values.add("'" + value + "'");
+            } else {
+                values.add(value.toString());
+            }
+        }
+
+        return String.join(",", values);
+    }
+
+    private String getColumnsWithoutId(Class<?> aClass) {
+        return Arrays.stream(aClass.getDeclaredFields())
+                .filter(f -> !f.isAnnotationPresent(Id.class))
+                .filter(f -> f.isAnnotationPresent(Column.class))
+                .map(f -> f.getAnnotationsByType(Column.class))
+                .map(a -> a[0].name())
+                .collect(Collectors.joining(","));
+    }
+
+    private String getTableName(Class<?> aClass) {
+        Entity[] entityAnnotations = aClass.getAnnotationsByType(Entity.class);
+
+        if (entityAnnotations.length == 0) {
+            throw new UnsupportedOperationException("Class must be Entity!");
+        }
+
+        return entityAnnotations[0].name();
     }
 
     @Override
@@ -57,7 +117,7 @@ public class EntityManager<E> implements DBContext<E> {
     }
 
     private Field getIdColumn(Class<?> entity) {
-       return Arrays.stream(entity.getDeclaredFields())
+        return Arrays.stream(entity.getDeclaredFields())
                 .filter(f -> f.isAnnotationPresent(Id.class))
                 .findFirst()
                 .orElseThrow(() -> new UnsupportedOperationException(
