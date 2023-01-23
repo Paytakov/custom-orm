6f6681a20ea5867455a5b792100c5594cf7f26c8
Paytakov <emo_paitakov@abv.bg>
2023-01-23 22:05:00 +0200

    add create table

--- src/main/java/orm/EntityManager.java
+++ src/main/java/orm/EntityManager.java
@@ -3,9 +3,9 @@ package orm;
 import annotation.Column;
 import annotation.Entity;
 import annotation.Id;
-
 import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -22,6 +22,19 @@ public class EntityManager<E> implements DBContext<E> {
         this.connection = connection;
     }
 
+    public void doCreate(Class<E> entityClass) throws SQLException {
+        String tableName = getTableName(entityClass);
+        String fieldsWithTypes = getSQLFieldsWithTypes(entityClass);
+
+        String createQuery = String.format("CREATE TABLE %s (" +
+                        "id INT PRIMARY KEY AUTO_INCREMENT, %s)",
+                tableName, fieldsWithTypes);
+
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(createQuery);
+        preparedStatement.execute();
+    }
+
     @Override
     public boolean persist(E entity) throws IllegalAccessException, SQLException {
         Field primaryKey = getIdColumn(entity.getClass());
@@ -33,6 +46,72 @@ public class EntityManager<E> implements DBContext<E> {
         }
 
         return doUpdate(entity, primaryKey);
+    }
+
+    @Override
+    public Iterator<E> find(Class<E> table) {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> find(Class<E> table, String where) {
+        return null;
+    }
+
+    @Override
+    public E findFirst(Class<E> table) {
+        return null;
+    }
+
+    @Override
+    public E findFirst(Class<E> table, String where) {
+        return null;
+    }
+
+    private Field getIdColumn(Class<?> entity) {
+        return Arrays.stream(entity.getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(Id.class))
+                .findFirst()
+                .orElseThrow(() -> new UnsupportedOperationException(
+                        "Entity does not have primary key!"));
+    }
+
+    private String getTableName(Class<?> aClass) {
+        Entity[] entityAnnotations = aClass.getAnnotationsByType(Entity.class);
+
+        if (entityAnnotations.length == 0) {
+            throw new UnsupportedOperationException("Class must be Entity!");
+        }
+
+        return entityAnnotations[0].name();
+    }
+
+    private String getSQLFieldsWithTypes(Class<E> entityClass) {
+        return Arrays.stream(entityClass.getDeclaredFields())
+                .filter(f -> !f.isAnnotationPresent(Id.class))
+                .filter(f -> f.isAnnotationPresent(Column.class))
+                .map(field -> {
+                    String fieldName = field.getAnnotationsByType(Column.class)[0].name();
+                    Class<?> type = field.getType();
+                    String sqlType = getSQLType(type);
+
+                    return fieldName + " " + sqlType;
+                })
+                .collect(Collectors.joining(","));
+
+    }
+
+    private String getSQLType(Class<?> fieldType) {
+        String type = "";
+        if (fieldType == Integer.class || fieldType == int.class) {
+            type = "INT";
+        } else if (fieldType == String.class) {
+            type = "VARCHAR(200)";
+        } else if (fieldType == LocalDate.class) {
+            type = "DATE";
+        }
+
+        return type;
     }
 
     private boolean doUpdate(E entity, Field primaryKey) {
@@ -84,43 +163,5 @@ public class EntityManager<E> implements DBContext<E> {
                 .map(f -> f.getAnnotationsByType(Column.class))
                 .map(a -> a[0].name())
                 .collect(Collectors.joining(","));
-    }
-
-    private String getTableName(Class<?> aClass) {
-        Entity[] entityAnnotations = aClass.getAnnotationsByType(Entity.class);
-
-        if (entityAnnotations.length == 0) {
-            throw new UnsupportedOperationException("Class must be Entity!");
-        }
-
-        return entityAnnotations[0].name();
-    }
-
-    @Override
-    public Iterator<E> find(Class<E> table) {
-        return null;
-    }
-
-    @Override
-    public Iterator<E> find(Class<E> table, String where) {
-        return null;
-    }
-
-    @Override
-    public E findFirst(Class<E> table) {
-        return null;
-    }
-
-    @Override
-    public E findFirst(Class<E> table, String where) {
-        return null;
-    }
-
-    private Field getIdColumn(Class<?> entity) {
-        return Arrays.stream(entity.getDeclaredFields())
-                .filter(f -> f.isAnnotationPresent(Id.class))
-                .findFirst()
-                .orElseThrow(() -> new UnsupportedOperationException(
-                        "Entity does not have primary key!"));
     }
 }
